dd944813234be12df1d46d7099194f9d6ad4aa7c
Nandhini M <nandhinimm4706@gmail.com>
2026-05-27 10:29:15 +0530

    Task

--- src/Service/Emp_Service.java
+++ src/Service/Emp_Service.java
@@ -4,6 +4,7 @@ import Model.Employee;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Emp_Service {
     Connection con;
@@ -32,5 +33,25 @@ public class Emp_Service {
             System.out.println("Employee not added");
         }
         ps.close();
+    }
+
+    public void viewEmployee(Employee emp) throws  Exception{
+        String query="Select * from employee";
+        PreparedStatement ps= con.prepareStatement(query);
+        ResultSet rs=ps.executeQuery();
+        while(rs.next()){
+            System.out.println("Id : " +rs.getInt("empId"));
+            System.out.println("Name : " +rs.getInt("empName"));
+            System.out.println("EmailId : " +rs.getInt("emailId"));
+            System.out.println("Mobile No : " +rs.getInt("mobileNo"));
+            System.out.println("City : " +rs.getInt("city"));
+            System.out.println("State : " +rs.getInt("state"));
+            System.out.println("Date Of Join : " +rs.getInt("date_of_join"));
+            System.out.println("Department Id : " +rs.getInt("deptId"));
+            System.out.println("Project Id : " +rs.getInt("projectId"));
+            System.out.println("Status : " +rs.getInt("emp_status"));
+            System.out.println("Experience : " +rs.getInt("experience"));
+            System.out.println("-------------------------------------------------------");
+        }
     }
 }
